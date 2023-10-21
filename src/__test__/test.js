b95f4b5ce4912b90ea45938083bb4ecc84f654b8
Paul Solovey <p.v.solovey@ya.ru>
2023-10-21 13:39:10 +0300

    Eslint errors fixed

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -1,29 +1,29 @@
 import sortCharacters from '../health-sort';
 
 test('health sorting whith not.toBe', () => {
-  let charactersList = [
-    {name: 'мечник', health: 10},
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
-];
+  const charactersList = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
   const sortedCharacters = [
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
-    {name: 'мечник', health: 10},
-  ]
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
   expect(sortCharacters(charactersList)).not.toBe(sortedCharacters);
 });
 
 test('health sorting whith toEqual', () => {
-  let charactersList = [
-    {name: 'мечник', health: 10},
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
-];
+  const charactersList = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
   const sortedCharacters = [
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
-    {name: 'мечник', health: 10},
-  ]
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
   expect(sortCharacters(charactersList)).toEqual(sortedCharacters);
 });
